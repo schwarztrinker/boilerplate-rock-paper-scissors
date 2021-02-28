464421420edc1c2efaa72cccdcee0960fc5106b8
Martin Stöcker <martin.stoecker99@gmail.com>
2021-02-28 13:43:04 +0100

    inits

--- RPS.py
+++ RPS.py
@@ -1,10 +1,30 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+
+import numpy as np
+import os
+import time
+
+
+
+
+    
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
+
+    #safePlayToFile('quincy.txt', prev_play)
 
     guess = "R"
     if len(opponent_history) > 2:
         guess = opponent_history[-2]
 
     return guess
+
+
+# Function for saving plays of the opponent to a file
+def safePlayToFile(filename, prev_play):
+    outF = open(filename, "a")
+    outF.write(prev_play)
+    outF.close()
--- main.py
+++ main.py
@@ -3,10 +3,26 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+
+import numpy as np
+import os
+import time
+
+###
+plays = open('quincy.txt', 'rb').read().decode(encoding='utf-8')
+print('Length of text: {} characters'.format(len(plays)))
+vocab = sorted(set(plays))
+print('{} unique characters'.format(len(vocab)))
+
+chars = tf.strings.unicode_split(plays, input_encoding='UTF-8')
+
+###
+#play(player, quincy, 5000)
+#play(player, abbey, 1000)
+#play(player, kris, 1000)
+#play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
@@ -17,4 +33,4 @@ play(player, mrugesh, 1000)
 
 
 # Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
